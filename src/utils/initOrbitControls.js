4c88e2d9f7c3546a854c8946729e08372b346aff
Dugrais Emmanuel <emmanuel.dugrais@gmail.com>
2023-12-09 22:48:10 +0100

    Updates

--- src/utils/initOrbitControls.js
+++
@@ -1,8 +0,0 @@
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { camera, renderer } from '../../main.js'
-
-export default function initOrbitControls() {
-    const orbitControls = new OrbitControls(camera, renderer.domElement)
-    orbitControls.update()
-    return orbitControls
-}
